0709be6f866efc6a4ec913f948daaa602b418010
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-03-20 21:44:10 +0900

    Feat : add vga text mode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use core::panic::PanicInfo;
 static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -11,13 +12,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
